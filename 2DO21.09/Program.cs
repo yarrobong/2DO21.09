6560520b3b4e664565211c499c9c8bfe2458eab6
yarrobong <oneboardsholi@gmail.com>
2023-09-21 21:54:02 +0500

    Добавьте файлы проекта.

---
+++ 2DO21.09/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("x1 = ");
+string input = Console.ReadLine();
+if (int.TryParse(input, out int x1))
+{
+    Console.WriteLine("y1 = ");
+    input = Console.ReadLine();
+    if (int.TryParse(input, out int y1))
+    {
+        Console.WriteLine("x2 = ");
+        input = Console.ReadLine();
+        if (int.TryParse(input, out int x2))
+        {
+            Console.WriteLine("y2 = ");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out int y2))
+            {
+                Console.WriteLine("Координаты первой точки: " + "(" + x1 + ";" + y1 + ")");
+                Console.WriteLine("Координаты второй точки: " + "(" + x2 + ";" + y2 + ")");
+
+                Double k = (y1 - y2) / (x1 - x2);
+
+                Double b = y2 - k * x2;
+
+                Console.WriteLine(k);
+                Console.WriteLine(b);
+
+                Console.WriteLine($"Уравнение прямой: y = {k}x + {b}");
+
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели некорректное значение для y2");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели некорректное значение для x2");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение для y1");
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели некорректное значение для x1");
+}
